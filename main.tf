b4020522a90463a328f87a83ac7227f278f1354f
kimjoshjp <kimjoshjp@gmail.com>
2020-07-23 20:08:41 +1000

    Update main.tf

--- main.tf
+++ main.tf
@@ -10,10 +10,10 @@ provider "azurerm" {
   version = "~>2.0"
   features {}
   
-  SUBSCRIPTION_ID = var.ARM_SUBSCRIPTION_I
-  CLIENT_ID = var.ARM_CLIENT_ID
-  CLIENT_SECRET = var.ARM_CLIENT_SECRET
-  TENANT_ID = var.ARM_TENANT_ID
+  ARM_SUBSCRIPTION_ID = var.ARM_SUBSCRIPTION_I
+  ARM_CLIENT_ID = var.ARM_CLIENT_ID
+  ARM_CLIENT_SECRET = var.ARM_CLIENT_SECRET
+  ARM_TENANT_ID = var.ARM_TENANT_ID
   
 }
 resource "azurerm_resource_group" "rg" {
